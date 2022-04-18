5565a346b3dc3a290f81c27fd97fe891fef4da3f
NobodyLifeKEK <97799987+NobodyLifeKEK@users.noreply.github.com>
2022-04-18 12:42:30 +0300

    Update Math.php

--- Math.php
+++ Math.php
@@ -1,22 +1,25 @@
 <?php
-function numbers($oneNum, $twoNum) {
+function evenNumber($oneNum, $twoNum) {
     $randNum = rand($oneNum, $twoNum);
-
     if(($randNum % 2) == 0){
         return $randNum;
     }
 }
 
-function numbersTwo($oneNum, $twoNum) {
+function oddNumber($oneNum, $twoNum) {
     $randNum = rand($oneNum, $twoNum);
     if(($randNum % 2) == 0){
+        $evNum = 'ошибка';
     }else{
         return $randNum;
     }
 }
 
-$oneNum = 1;
-$twoNum = 20;
-echo numbers($oneNum, $twoNum).'<br>';
-echo numbersTwo($oneNum, $twoNum);
+$minNum = 1;
+$maxNum = 20;
+$evenNum = evenNumber($minNum, $maxNum);
+$oddNum = oddNumber($minNum, $maxNum);
+echo $oddNum.'<br>';
+echo $evenNum;
+
 ?>
