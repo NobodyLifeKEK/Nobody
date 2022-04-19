dfe530aae1e17e9e39c3063e6f1a2bb1737448c4
NobodyLifeKEK <97799987+NobodyLifeKEK@users.noreply.github.com>
2022-04-19 11:51:21 +0300

    Update Math.php

--- Math.php
+++ Math.php
@@ -3,19 +3,19 @@
 function  recursionOdd($numOne, $numTwo){
     $randNum = rand($numOne,$numTwo);
     if (($randNum % 2) != 0){
-        echo $randNum;
+        $randNum;
     }
     else(recursionOdd ($numOne,$numTwo));
-
+    return($randNum);
 }
 
 function  recursionEven($numOne,$numTwo){
     $randNum = rand($numOne,$numTwo);
     if (($randNum % 2) == 0){
-        echo $randNum;
+        $randNum;
     }
     else(recursionEven ($numOne,$numTwo));
-
+   return($randNum);
 }
 
 $numOne = 0;
