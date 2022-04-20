81363bb71c52cc3d2cbaf68c63ad410d0c9395f9
NobodyLifeKEK <97799987+NobodyLifeKEK@users.noreply.github.com>
2022-04-20 18:11:12 +0300

    Update Math.php

--- Math.php
+++ Math.php
@@ -18,11 +18,48 @@ function  recursionOdd($numOne, $numTwo) {
     else{
         return recursionOdd($numOne, $numTwo);
     }
+    }
+
+function massiveRand($Num) {
+    $randArr = [];
+    for ($i = 0; $i < $Num; $i++) {
+        $randArr[$i] = recursionEven(2,80);
+    }
+    return $randArr;
 }
 
-$numOne = 0;
-$numTwo = 20;
+function deletedArray(int $a, int $b, array $arr){
+
+    $arr1 = [];
+    $length = count($arr);
+    for($i = 0; $i < $length; $i++) {
+        if($b > $length) {
+            echo "Число вышло за массив";
+            break;
+        }
+        if($i < $a || $i > $b){
+            $arr1[] = $arr[$i];
+        }
+    }
+    return $arr1;
+}
+
+function arrSub($arr) {
+    $result = $arr[0];
+    for ($i = 1; $i < count($arr); $i++) {
+        $result -= $arr[$i];
+    }return $result;
+}
+
+$a = 3;
+$b = 6;
+$numOne = 2;
+$numTwo = 80;
 $oddNum = recursionOdd($numOne,$numTwo);
 $evenNum = recursionEven($numOne,$numTwo);
+$mass = massiveRand(10);
+$deletedArr = deletedArray($a, $b, $mass);
 echo $oddNum.'<br>';
-echo $evenNum;
+echo $evenNum.'<br>';
+print_r($deletedArr);
+print_r(arrSub($deletedArr));
